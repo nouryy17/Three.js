0fb920c54004708e1bb1bd7e81bbf2c72402dc57
nouryy17 <dacinour16@gmail.com>
2024-07-09 18:37:24 +0200

    add code

--- three.js/main.js
+++ three.js/main.js
@@ -1,24 +1,25 @@
+import gsap from 'gsap'
 import * as THREE  from "three"
-import * as dat from "dat.gui";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
+import * as dat from "dat.gui";
 const gui = new dat.GUI()
 const world = {
   plane: {
-    width: 10,
-    height: 10,
-    widthSegments: 10,
-    heightSegments: 10
+    width: 400,
+    height: 400,
+    widthSegments: 50,
+    heightSegments: 50
   }
 }
-gui.add(world.plane, 'width', 1, 20).onChange(generatePlane)
+gui.add(world.plane, 'width', 1, 500).onChange(generatePlane)
 
-gui.add(world.plane, 'height', 1, 20).onChange(generatePlane)
-gui.add(world.plane, 'widthSegments', 1, 50).onChange(generatePlane)
-gui.add(world.plane, 'heightSegments', 1, 50).onChange(generatePlane)
+gui.add(world.plane, 'height', 1, 500).onChange(generatePlane)
+gui.add(world.plane, 'widthSegments', 1, 100).onChange(generatePlane)
+gui.add(world.plane, 'heightSegments', 1, 100).onChange(generatePlane)
 
 function generatePlane() {
-  plane.geometry.dispose()
-  plane.geometry = new THREE.PlaneGeometry(
+  planeMesh.geometry.dispose()
+  planeMesh.geometry = new THREE.PlaneGeometry(
     world.plane.width,
     world.plane.height,
     world.plane.widthSegments,
@@ -26,89 +27,167 @@ function generatePlane() {
   )
 
   // vertice position randomization
-  const {array} = plane.geometry.attributes.position
-  for (let i = 0; i < array.length; i+=3) {
-    
+  const { array } = planeMesh.geometry.attributes.position
+  const randomValues = []
+  for (let i = 0; i < array.length; i++) {
+    if (i % 3 === 0) {
       const x = array[i]
       const y = array[i + 1]
       const z = array[i + 2]
 
-     array[i + 2]= z + Math.random()
-    
+      array[i] = x + (Math.random() - 0.5) * 3
+      array[i + 1] = y + (Math.random() - 0.5) * 3
+      array[i + 2] = z + (Math.random() - 0.5) * 3
+    }
 
-    } }
+    randomValues.push(Math.random() * Math.PI * 2)
+  }
 
-const raycaster = new THREE.WebGLRenderList()
-// انشاء مشهد
-const scene = new THREE.Scene();
-// انشاء كاميرا
-const camera = new THREE.PerspectiveCamera( 75,
-   window.innerWidth / window.innerHeight,
-    0.1,
-    1000 );
-// انشاء تقدم
+  planeMesh.geometry.attributes.position.randomValues = randomValues
+  planeMesh.geometry.attributes.position.originalPosition =
+    planeMesh.geometry.attributes.position.array
 
-const renderer = new THREE.WebGLRenderer();
+  const colors = []
+  for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+    colors.push(0, 0.19, 0.4)
+  }
 
-//نعطيه كامل العرض و كامل الارتفاع
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-renderer.setPixelRatio(devicePixelRatio)
-// استدعاء البودي
-
-document.body.appendChild( renderer.domElement );
-new OrbitControls(camera,renderer,domElement)
-//  اي شكل مكون من حاجتين = من هندسة (الابعاد) + المادة ( الالوان)
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// عرض الطول الارتفاع
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// الان نربط بين المادة و الهندسة ليصبح لدينا شكل (شبكة)
-// scene.add( cube );
-//  الان نضيف الشبكة للمشهد
-camera.position.z = 5;
-
-// اضافة الطائرة
-const geometryPlane = new THREE.PlaneGeometry( 5, 5,10, 10 );
-const materialPlane = new THREE.MeshNormalMaterial({
-  color:0xff0000,
-  side: THREE.DoubleSide,
-  flatShading: THREE.FlatShading
-})
-const plane = new THREE.Mesh( geometryPlane, materialPlane );
-scene.add(plane);
-const { array }=plane.geometry.attributes.position
-for (let i = 0; i < array.length; i+= 3) {
-	
-	const x =  array[i]
-	const y = array[i +1]
-	const z = array[i+2]
-	array[i +2 ]= z + Math.random()
+  planeMesh.geometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(new Float32Array(colors), 3)
+  )
 }
-// add ligth
-const ligth =new THREE.DirectionalLight( 0x00ff00 ,1)
-ligth.position(0,0,1)
-scene.add(ligth)
-// add backligth
-const backligth =new THREE.DirectionalLight( 0x00ff00 ,1)
-ligth.position(0,0,-1)
-scene.add(backligth)
+
+const raycaster = new THREE.Raycaster()
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+)
+const renderer = new THREE.WebGLRenderer()
+
+renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(devicePixelRatio)
+document.body.appendChild(renderer.domElement)
+
+new OrbitControls(camera, renderer.domElement)
+camera.position.z = 50
+
+const planeGeometry = new THREE.PlaneGeometry(
+  world.plane.width,
+  world.plane.height,
+  world.plane.widthSegments,
+  world.plane.heightSegments
+)
+const planeMaterial = new THREE.MeshPhongMaterial({
+  side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading,
+  vertexColors: true
+})
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+scene.add(planeMesh)
+generatePlane()
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(0, -1, 1)
+scene.add(light)
+
+const backLight = new THREE.DirectionalLight(0xffffff, 1)
+backLight.position.set(0, 0, -1)
+scene.add(backLight)
+
 const mouse = {
   x: undefined,
   y: undefined
 }
-// انشاء انيم الدوران
+
+let frame = 0
 function animate() {
 
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
-requestAnimationFrame(animate)
-renderer.render( scene, camera );
-raycaster.setFromCamera(mouse, camera)
-const intersects=raycaster.intersectsObject(plane)
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+  raycaster.setFromCamera(mouse, camera)
+  frame += 0.01
 
+  const {
+    array,
+    originalPosition,
+    randomValues
+  } = planeMesh.geometry.attributes.position
+  for (let i = 0; i < array.length; i += 3) {
+    // x
+    array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.01
+
+    // y
+    array[i + 1] =
+      originalPosition[i + 1] + Math.sin(frame + randomValues[i + 1]) * 0.001
+  }
+
+  planeMesh.geometry.attributes.position.needsUpdate = true
+
+  const intersects = raycaster.intersectObject(planeMesh)
+  if (intersects.length > 0) {
+    const { color } = intersects[0].object.geometry.attributes
+
+    // vertice 1
+    color.setX(intersects[0].face.a, 0.1)
+    color.setY(intersects[0].face.a, 0.5)
+    color.setZ(intersects[0].face.a, 1)
+
+    // vertice 2
+    color.setX(intersects[0].face.b, 0.1)
+    color.setY(intersects[0].face.b, 0.5)
+    color.setZ(intersects[0].face.b, 1)
+
+    // vertice 3
+    color.setX(intersects[0].face.c, 0.1)
+    color.setY(intersects[0].face.c, 0.5)
+    color.setZ(intersects[0].face.c, 1)
+
+    intersects[0].object.geometry.attributes.color.needsUpdate = true
+
+    const initialColor = {
+      r: 0,
+      g: 0.19,
+      b: 0.4
+    }
+
+    const hoverColor = {
+      r: 0.1,
+      g: 0.5,
+      b: 1
+    }
+
+    gsap.to(hoverColor, {
+      r: initialColor.r,
+      g: initialColor.g,
+      b: initialColor.b,
+      duration: 1,
+      onUpdate: () => {
+        // vertice 1
+        color.setX(intersects[0].face.a, hoverColor.r)
+        color.setY(intersects[0].face.a, hoverColor.g)
+        color.setZ(intersects[0].face.a, hoverColor.b)
+
+        // vertice 2
+        color.setX(intersects[0].face.b, hoverColor.r)
+        color.setY(intersects[0].face.b, hoverColor.g)
+        color.setZ(intersects[0].face.b, hoverColor.b)
+
+        // vertice 3
+        color.setX(intersects[0].face.c, hoverColor.r)
+        color.setY(intersects[0].face.c, hoverColor.g)
+        color.setZ(intersects[0].face.c, hoverColor.b)
+        color.needsUpdate = true
+      }
+    })
+  }
 }
+
 animate()
+
 addEventListener('mousemove', (event) => {
   mouse.x = (event.clientX / innerWidth) * 2 - 1
   mouse.y = -(event.clientY / innerHeight) * 2 + 1
